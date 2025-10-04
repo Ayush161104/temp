e90cca0af27332d9ef56e7c1d474b69985c34c8c
prachipokhare <prachipokhare@gmail.com>
2025-10-04 21:27:51 +0530

    fixed

--- temp_converter.py
+++ temp_converter.py
@@ -1,10 +1,10 @@
 # temp_converter.py
 
 def c_to_f(c):
-    return c * 1.8  # ❌ BUG: Missing +32
+    return c * 1.8 + 32
 
 def f_to_c(f):
-    return (f - 32) * 1.8  # ❌ BUG: Should be /1.8
+    return (f - 32) / 1.8 
 
 print("25C in F:", c_to_f(25))
 print("77F in C:", f_to_c(77))
